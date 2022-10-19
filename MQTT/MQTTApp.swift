efe1278ab6d346a32a4f53c7a9b28613b54925b2
Vincenzo Caruso <enzo@carusovincenzo.com>
2022-10-19 15:00:52 +0200

    Initial Commit

---
+++ MQTT/MQTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MQTTApp.swift
+//  MQTT
+//
+//  Created by Vincenzo Caruso on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MQTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
